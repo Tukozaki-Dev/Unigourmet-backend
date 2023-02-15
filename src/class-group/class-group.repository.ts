b238a5f882487178cb4efc658d0390d03e72a65d
GrazielyTukozaki <grazielybasilio@hotmail.com>
2023-02-15 15:46:22 -0300

    created the get, getById, patch and delete methods at classgroup endpoint

--- src/class-group/class-group.repository.ts
+++ src/class-group/class-group.repository.ts
@@ -1,6 +1,7 @@
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateClassGroupDto } from './dto/create-class-group.dto';
+import { UpdateClassGroupDto } from './dto/update-class-group.dto';
 import { ClassGroup, ClassGroupDocument } from './schemas/class-group.schema';
 
 export class ClassGroupRepository {
@@ -11,5 +12,25 @@ export class ClassGroupRepository {
   async create(createClassGroupDto: CreateClassGroupDto) {
     const createdClassGroup = new this.classGroupModel(createClassGroupDto);
     return await createdClassGroup.save();
+  }
+
+  async findAll() {
+    return await this.classGroupModel.find({});
+  }
+
+  async findOne(id: string) {
+    return await this.classGroupModel.findById(id);
+  }
+
+  async update(id: string, updateClassGroupDto: UpdateClassGroupDto) {
+    await this.classGroupModel.findOneAndUpdate(
+      { _id: id },
+      updateClassGroupDto,
+    );
+    return this.findOne(id);
+  }
+
+  async remove(id: string) {
+    return await this.classGroupModel.findByIdAndDelete(id);
   }
 }
--- src/class-group/dto/update-class-group.dto.ts
+++ src/class-group/dto/update-class-group.dto.ts
@@ -1,4 +1,66 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { Type } from 'class-transformer';
+import {
+  IsArray,
+  IsNumber,
+  IsOptional,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
 import { CreateClassGroupDto } from './create-class-group.dto';
 
-export class UpdateClassGroupDto extends PartialType(CreateClassGroupDto) {}
+export class Disciplines {
+  @IsOptional()
+  @IsString()
+  _id: string;
+
+  @IsOptional()
+  @IsString()
+  name: string;
+}
+
+export class Students {
+  @IsOptional()
+  @IsString()
+  _id: string;
+
+  @IsOptional()
+  @IsString()
+  name: string;
+}
+
+export class UpdateClassGroupDto extends PartialType(CreateClassGroupDto) {
+  @IsOptional()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  registerCode: string;
+
+  @IsOptional()
+  @IsNumber()
+  semester: number;
+
+  @IsOptional()
+  @IsString()
+  shift: string;
+
+  @IsOptional()
+  @IsString()
+  category: string;
+
+  @IsOptional()
+  @IsArray()
+  @ValidateNested({
+    each: true,
+  })
+  @Type(() => Disciplines)
+  disciplines: Disciplines[];
+
+  @IsOptional()
+  @IsArray()
+  @Type(() => Students)
+  @ValidateNested()
+  students: Students[];
+}
