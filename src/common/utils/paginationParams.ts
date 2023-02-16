59c8fcc32b49a35f82a5f5ab02a87bb10baba86a
Izabela Soares <soares.izabela96@gmail.com>
2023-02-16 11:13:15 -0300

    fixed pagination

--- src/common/utils/paginationParams.ts
+++ src/common/utils/paginationParams.ts
@@ -1,11 +1,16 @@
-import { Type } from "class-transformer";
-import { IsInt, IsOptional } from "class-validator";
-
+import { IsNumber, Min, IsOptional } from 'class-validator';
+import { Type } from 'class-transformer';
+ 
 export class PaginationParams {
-
-    @IsOptional()
-    @IsInt()
-    @Type(() => Number)
-    page: number;
-
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  @Min(0)
+  skip?: number;
+ 
+  @IsOptional()
+  @Type(() => Number)
+  @IsNumber()
+  @Min(1)
+  limit?: number;
 }
